d6f6a63f060c624022de1d7ed04d8abe1ec51a8b
Miguel Arce <correo.arce@gmail.com>
2022-10-03 18:59:31 -0300

    templates

--- informe/views.py
+++ informe/views.py
@@ -1,1 +1,10 @@
+from urllib import response
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+   
+    return render(request, "home.html")
 
--- informes/urls.py
+++ informes/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('informe.urls')), 
     path('admin/', admin.site.urls),
     path('informe/', include('informe.urls')),
     path('publicador/', include('publicador.urls')),
