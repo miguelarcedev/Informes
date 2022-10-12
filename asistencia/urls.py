7ad77b4d591c9cedf7690fece76ab28637aee166
miguelarcedev <correo.arce@gmail.com>
2022-10-12 12:54:40 -0300

    totales-listo

--- asistencia/urls.py
+++ asistencia/urls.py
@@ -9,8 +9,8 @@ from . import views
 
 urlpatterns = [
     
-    path('lista_años/<str:entre_fin>/', views.lista_años, name='lista_años'),
-    path('lista_por_año/<int:año>/<str:entre_fin>/', views.lista_por_año, name='lista_por_año'),
+    path('lista_años/<str:entre_fin>/', views.lista_años, name='asistencia_años'),
+    path('lista_por_año/<int:año>/<str:entre_fin>/', views.lista_por_año, name='asistencia_por_año'),
     path('EntreSemana/<int:año>/', EntreSemanaPdf.as_view(), name='EntreSemana_pdf'),
     path('FinDeSemana/<int:año>/', FinDeSemanaPdf.as_view(), name='FinDeSemana_pdf'),
     
