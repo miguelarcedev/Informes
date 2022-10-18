10ac60f49272315fd1cba721cae6fa5c3d6881b9
Miguel Arce <correo.arce@gmail.com>
2022-10-18 20:43:26 -0300

    listados

--- informe/urls.py
+++ informe/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from informe.views import Tarjeta_grupo
+from informe.views import Tarjeta_grupo, Precursores, Inactivos
 from informe import views
+
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('tarjeta_grupo/<int:grupo>/', Tarjeta_grupo.as_view(), name='tarjeta_grupo'),
-    
+    path('precursores/', Precursores.as_view(), name='precursores'),
+    path('inactivos/', Inactivos.as_view(), name='inactivos'),
 ]
