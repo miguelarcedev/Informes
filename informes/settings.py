85d39fd0144228f31d25a5259be7d4c0e1b21d41
miguelarcedev <correo.arce@gmail.com>
2022-11-09 09:21:54 -0300

    deploy

--- informes/settings.py
+++ informes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/admin/login/?next=/admin/'
+
+django_heroku.settings(locals())
