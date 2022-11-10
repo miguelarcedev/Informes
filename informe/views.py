22dc5c9796d748d6592a17d77eae8d4aa832524f
miguelarcedev <correo.arce@gmail.com>
2022-11-10 08:59:57 -0300

    vaqueriendo

--- informe/views.py
+++ informe/views.py
@@ -16,7 +16,7 @@ def home(request):
     #grupos=Publicador.objects.filter(estado="Activo").values('grupo').order_by('grupo').annotate(suma=Sum('grupo'))
     grupos = []
     cantidad=Publicador.objects.filter(estado="Activo").aggregate(cantidad=Max('grupo'))
-    cantidad=int(cantidad['cantidad'])
+    cantidad=(cantidad['cantidad'])
     for i in range(1,cantidad+1):
         grupos.append(Publicador.objects.filter(grupo=i, estado="Activo"))
     return render(request, "home.html",{"grupos": grupos})
