aa430f5cadcd7c8c76040e5891bf36486e6ab62d
miguelarcedev <correo.arce@gmail.com>
2024-02-26 12:00:12 -0300

    depurando

--- informes/settings.py
+++ informes/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'informe',
     'asistencia',
     'import_export', 
+    
 
 ]
 
--- informes/wsgi.py
+++ informes/wsgi.py
@@ -12,10 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'informes.settings')
-
 application = get_wsgi_application()
 
-# EStas 3 lineas agrego para ver que pasa con el deploy en IIS, borrar si no es necesario
 
 
 
