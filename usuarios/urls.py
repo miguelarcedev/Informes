98ac4b7f19bee415240e4e3067aadb1fd1b151b1
unknown <correo.arce@gmail.com>
2025-08-18 00:25:24 -0300

    se elimino app usuario

--- usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-# usuarios/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registro/', views.registro, name='registro'),
-    path('activar/<uidb64>/<token>/', views.activar, name='activar'),
-]
