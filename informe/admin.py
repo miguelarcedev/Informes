b03105db5ee80f39eb3a56f1568be6ea1e7fbfe1
miguelarcedev <correo.arce@gmail.com>
2025-09-30 14:30:59 -0300

    panel admin para users staff

--- informe/admin.py
+++ informe/admin.py
@@ -5,42 +5,14 @@ from .models import Informe
 from publicador.models import Publicador
 
 class Informe_Resource(resources.ModelResource):
-
     class Meta:
         model = Informe
-
 class Informe_Admin(ImportExportModelAdmin):
     resource_class = Informe_Resource
     list_display = ('publicador','año', 'mes','participacion','estudios','auxiliar','horas',  'notas', 'servicio')
     list_filter = ['notas', 'año','mes']
     search_fields = ['publicador__apellido', 'publicador__nombre']
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs  # Ver todo
-        try:
-            publicador = Publicador.objects.get(user=request.user)
-            return qs.filter(publicador=publicador)
-        except Publicador.DoesNotExist:
-            return qs.none()  # No mostrar nada si el user no está vinculado
-
-    def has_change_permission(self, request, obj=None):
-        if request.user.is_superuser:
-            return True
-        if obj is None:
-            return True
-        return obj.publicador.user == request.user
-
-    def has_delete_permission(self, request, obj=None):
-        return self.has_change_permission(request, obj)
-
-    def has_view_permission(self, request, obj=None):
-        if request.user.is_superuser:
-            return True
-        if obj is None:
-            return True
-        return obj.publicador.user == request.user
+  
 
 admin.site.register(Informe, Informe_Admin)
 admin.site.site_header = "Panel de Administracion"
