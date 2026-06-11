577016a2fbb22eb5dec523cc9f1926e929659edd
miguelarcedev <correo.arce@gmail.com>
2026-06-11 09:04:23 -0300

    cambio de email_host_password

--- informes/settings.py
+++ informes/settings.py
@@ -234,5 +234,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'adm.cuentas.correo@gmail.com'
-EMAIL_HOST_PASSWORD = 'lhso hedx aqao ixox'
-
+#EMAIL_HOST_PASSWORD = 'lhso hedx aqao ixox'
+EMAIL_HOST_PASSWORD = 'vqbh muwm dtnw ural'
